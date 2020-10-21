2b45801d678c6cdc11409c0a8b7045c4071debbe
heena1999 <73223242+heena1999@users.noreply.github.com>
2020-10-21 19:41:37 +0530

    Add files via upload

---
+++ Product.java
@@ -0,0 +1,32 @@
+
+public class Product {
+	public static void main(String[] args) {
+	private int productid;
+	private String productname;
+	private String desc;
+	private double price;
+	public int getproductid() {
+		return productid;
+		}
+	public void setproductid(int productid) {
+		this.productid = productid;
+	}
+	public String getproductname() {
+		return productname;
+	}
+	public void setproductname(String productname) {
+		this.productname = productname;
+	}
+	public String getdesc() {
+		return desc;
+	}
+	public void setdesc(String desc) {
+		this.desc = desc;
+	}
+	public double getprice() {
+		return price;
+	}
+	public void setprice(double price) {
+		this.price = price;
+	}	
+}
